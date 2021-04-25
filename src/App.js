9b33ff2674df5465c675c9845719d620d06d540e
Gabriel Henton <gabehenton@gmail.com>
2021-04-25 14:09:34 -0700

    reducers and actions and other redux stuff set up but errors prevent rendering...

--- src/App.js
+++ src/App.js
@@ -4,37 +4,53 @@ import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { connect } from 'react-redux';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
+const App = (props) => {
   return (
-    <div className="boxes">
-      <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+    <div className='boxes'>
+      <div className='box'>
+        <Header car={props.car} />
+        <AddedFeatures car={props.car} />
       </div>
-      <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+      <div className='box'>
+        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car,
+    additionalFeatures: state.additionalFeatures,
+    additionalPrice: state.additionalPrice,
+  };
+};
+
+export default connect(mapStateToProps, {})(App);
+
+/*** READ from the redux store
+ *  const mapStateToProps = state => {
+ *    //map each value to be read from the store to a prop
+ *     return {
+ *       editing: state.editing,
+ *       title: state.title
+ *     };
+ *  };
+ *
+ * ** WRITE to the redux store
+ *   const mapDispatchToProps = dispatch => {
+ * // build a dispatching function from each action creator
+ *   return {
+ *     updateTitle: newTitle => dispatch(updateTitle(newTitle)),
+ *     toggleEditing: () => dispatch(toggleEditing()),
+ *     // other actions here
+ *   };
+ * };
+ *
+ * export default connect(mapStateToProps,mapDispatchToProps)(Title);
+ *
+ *
+ */
